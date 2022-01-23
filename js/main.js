f76d3b644652c636a56e9379b43605df84a34b43
mrshuhrat <shuhratadm1n@gmail.com>
2022-01-23 23:36:39 +0500

    Add AOS.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,20 @@
+// Call AOS function
+AOS.init();
+
 const elMenuOpen = findElement('.js-menu-open');
 const elMenuClose = findElement('.js-menu-close');
 const menu = findElement('.js-menu');
+
+const elContactBtn = findElement('.js-toggle-contact');
 
 elMenuOpen.addEventListener('click', () => {
   menu.classList.add('header__toggle-open');
 });
 
 elMenuClose.addEventListener('click', () => {
+  menu.classList.remove('header__toggle-open');
+});
+
+elContactBtn.addEventListener('click', () => {
   menu.classList.remove('header__toggle-open');
 });
